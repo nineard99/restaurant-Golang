eed27ad4514bcfd4368461f4924295dda9099a6f
nineard99 <angkoon.ardt@mail.kmutt.ac.th>
2025-07-16 19:05:56 +0700

    docker complete

--- backend_Golang/config/db.go
+++ backend_Golang/config/db.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/nineard99/restaurant-Golang/models"
@@ -30,13 +31,34 @@ func ConnectDB() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&charset=utf8mb4&loc=Local",
 		dbUser, dbPass, dbHost, dbPort, dbName)
 
-	// เชื่อมต่อฐานข้อมูล
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("Failed to connect database: %v", err)
+	// retry loop รอ mysql พร้อม
+	var db *gorm.DB
+	for i := 0; i < 10; i++ {
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			sqlDB, errPing := db.DB()
+			if errPing == nil && sqlDB.Ping() == nil {
+				log.Println("✅ Connected to database")
+				break
+			}
+		}
+		log.Println("⏳ Waiting for database to be ready... retrying in 3 seconds")
+		time.Sleep(3 * time.Second)
 	}
 
-	err = db.AutoMigrate(&models.User{}, &models.RestaurantUser{}, &models.Restaurant{}, &models.MenuItem{}, &models.Order{}, &models.OrderItem{}, models.SeatTable{})
+	if err != nil {
+		log.Fatalf("Failed to connect database after retries: %v", err)
+	}
+
+	err = db.AutoMigrate(
+		&models.User{},
+		&models.RestaurantUser{},
+		&models.Restaurant{},
+		&models.MenuItem{},
+		&models.Order{},
+		&models.OrderItem{},
+		models.SeatTable{},
+	)
 	if err != nil {
 		log.Fatalf("AutoMigrate failed: %v", err)
 	}
